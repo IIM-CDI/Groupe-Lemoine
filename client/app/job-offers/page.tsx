7d37a92eef741140d3ee4299dfd5865b0eba7257
MaxymMelnychuk <maxymmelnychuk100@gmail.com>
2026-03-28 14:18:09 +0100

    feat: implement job filtering by type and experience

--- client/app/job-offers/page.tsx
+++ client/app/job-offers/page.tsx
@@ -9,6 +9,8 @@ import { fetchStrapi } from "@/lib/strapi";
 export default function Offers() {
   const [jobs, setJobs] = useState<Job[]>([]);
   const [selectedJob, setSelectedJob] = useState<Job | null>(null);
+  const [type, setType] = useState<string>("");
+  const [experience, setExperience] = useState<string>("");
   
   useEffect(() => {
     fetchStrapi("/api/job-offers")
@@ -19,6 +21,13 @@ export default function Offers() {
         setSelectedJob(data?.data?.[0]);
       });
   }, []);
+
+  const filteredJobs = jobs.filter(job => {
+    const matchContrat = !type || job.type.toLowerCase() === type.toLowerCase();
+    const matchExp = !experience || job.experience === experience;
+    return matchContrat && matchExp;
+  });
+  
   return (
     <div className="flex flex-col p-12 gap-12 min-h-screen bg-cover bg-[url('/images/offers-bg.png')] items-center justify-center  font-san">
       <h1 className="text-6xl italic text-black">Trouver votre futur emploi</h1>
@@ -26,19 +35,31 @@ export default function Offers() {
         <select className="border border-neutral-400 ">
           <option>Location</option>
       </select>
-      <select className="border border-neutral-400">
-          <option>Type</option>
-      </select>
-      <select className="border border-neutral-400">
-          <option>Experience</option>
+        <select onChange={(e) => setType(e.target.value)} id="offre" name="offre" className="border border-neutral-400">
+          <option value="">Type</option>
+          <option value="cdi">CDI</option>
+          <option value="cdd">CDD</option>
+          <option value="stage">Stage</option>
+          <option value="alternance">Alternance</option>
+          <option value="freelance">Freelance</option>
+          <option value="autre">Autre</option>
+        </select>
+      <select onChange={(e) => setExperience(e.target.value)} className="border border-neutral-400">
+          <option value="">Experience</option>
+          <option value="De 0 à 2 ans">De 0 à 2 ans</option>
+          <option value="De 3 à 5 ans">De 3 à 5 ans</option>
+          <option value="De 6 à 10 ans">De 6 à 10 ans</option>
+          <option value="Plus de 10 ans">Plus de 10 ans</option>
       </select>
       </div>
       <div className="flex gap-4 w-full max-w-6xl">
         <div className="flex flex-col gap-6 flex-1 max-h-screen overflow-y-scroll">
-        {jobs.map((job) => (
+        {filteredJobs.map((job) => (
           <OfferCard
             key={job.id}
             title={job.title}
+            type={job.type}
+            experience={job.experience}
             description={job.description}
             location={job.location}
             date={job.createdAt}
--- client/components/job-offers/offerDetail.tsx
+++ client/components/job-offers/offerDetail.tsx
@@ -9,17 +9,13 @@ export default function OfferDetail({ job }: OfferDetailProps) {
   return (
     <div className="p-6 bg-neutral-100 text-black flex flex-col gap-4 max-w-full">
       <h2 className="text-3xl font-bold">{job.title}</h2>
-      <p className="text-lg text-black whitespace-pre-wrap">{job.description}</p>
-
-      <div className="flex gap-4 text-black mt-4">
+        <div className='flex gap-4'>
+            <span className="text-md bg-neutral-200 w-max text-neutral-600 text-xs px-3 py-1 rounded-md">{job.type}</span>
+            <span className="text-md bg-neutral-200 w-max text-neutral-600 text-xs px-3 py-1 rounded-md">{job.experience}</span>
+        </div>
         <p><strong>Lieu :</strong> {job.location}</p>
-        <p><strong>Type :</strong> {job.type}</p>
-        <p><strong>Expérience :</strong> {job.experience}</p>
-      </div>
-
-      <p className="mt-2 text-gray-500">
-        {timeAgo(job.createdAt)}
-      </p>
+      <p className="text-lg text-black whitespace-pre-wrap">{job.description}</p>
+      <p className="mt-2 text-gray-500">{timeAgo(job.createdAt)}</p>
     </div>
   );
 }
