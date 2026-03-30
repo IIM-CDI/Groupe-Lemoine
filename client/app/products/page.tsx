1fd227a4a9fd2c1e4711993300011673b229b3ea
Celena L'entete <claire.lentete@outlook.com>
2026-03-30 22:01:55 +0200

    feat: products nav

--- client/app/products/page.tsx
+++ client/app/products/page.tsx
@@ -5,25 +5,24 @@ import Image from 'next/image';
 
 export default function products() {
   return (
-    <div className="max-h-100">
-      <h1>Title</h1>
-      <p>Description</p>
+    <div className="">
+      <h1 className="text-center text-(--secondary) text-3xl ">Title</h1>
+      <p className="text-center text-lg text-gray-400">Description</p>
 
       <div className="">
-        <div className="flex flex-row">
-            <button>P!</button>
-            <button>p2</button>
-            <button>p3</button>
-            <button>p4</button>
+        <div className="flex flex-row place-self-center bg-gray-400 p-4 rounded-full gap-5" >
+          <button className="products-button">P1</button>
+          <button className="products-button">p2</button>
+          <button className="products-button"> p3</button>
+          <button className="products-button">p4</button>
         </div>
-        <div className="">
+        <div className="w-screen bg-red-100" >
           <Image
             src="/logo-vertical-lemoine.png"
-            width={1000}
-            height={1000}
+            width={50}
+            height={50}
             alt="Picture of the author"
           />
-          <h2>Coton machin truc</h2>
         </div>
       </div>
     </div>
